263656766d2dc3d0548099ba93bc5214a7b327c9
William Mak <william.mak@hotmail.ca>
2016-01-23 00:30:29 -0500

    adding the code to do the alarms

--- main.py
+++ main.py
@@ -1,23 +1,53 @@
-from datetime import datetime, timedelta
+from apscheduler.scheduler import Scheduler
+from datetime import datetime, timedelta, date
 from flask import Flask, g, request
 import json
+import time
 app = Flask(__name__)
 base_url = "/api/"
 ignore_urls = [base_url, "/static/<path:filename>"]
+sched = Scheduler()
+sched.start()
+
+def wakeup():
+    # Put the code here to wake someone up
+    pass
+
+def sleep():
+    # Put the code here to remind someone to sleep
+    print("go to sleep!")
+    return
 
 class Alarm():
     def __init__(self, time_format):
-        self.time = None
+        self.sleep = None
+        self.alarms = {}
         self.time_format = time_format
 
     def set_alarm(self, time):
         strtime = time.decode("utf-8")
-        self.time = datetime.strptime(strtime, self.time_format)
-        self.wakeup = self.time - timedelta(hours = 8)
+        today = date.today()
+        self.wakeup = datetime.strptime(strtime, self.time_format)
+        self.wakeup = self.wakeup.replace(
+            year = today.year,
+            month = today.month,
+            day = today.day)
+        self.sleep = self.wakeup - timedelta(hours = 8)
+        if self.wakeup < datetime.now():
+            self.wakeup += timedelta(hours = 24)
+            self.sleep = self.wakeup + timedelta(hours = 8)
+        if ("sleep" in self.alarms):
+            self.alarms.sleep.remove()
+        if ("wakeup" in self.alarms):
+            self.alarms.wakeup.remove()
+        self.alarms = {
+            "sleep" : sched.add_date_job(sleep, self.sleep, []),
+            "wakeup" : sched.add_date_job(wakeup, self.wakeup, []),
+        }
 
     def __str__(self):
         return json.dumps({
-            "wakeup" : self.time.strftime(self.time_format),
+            "wakeup" : self.sleep.strftime(self.time_format),
             "sleep" : self.wakeup.strftime(self.time_format),
         })
 
@@ -31,6 +61,7 @@ def list_urls():
 
 @app.route(base_url + "toggle")
 def toggle_switch():
+    #exec('terrence put the toggle command here')
     return "this endpoint will eventuall toggle the light on/off"
 
 @app.route(base_url + "alarm", methods=["GET", "POST", "PUT"])
