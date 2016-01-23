cf8ec6801dd7c054481040caf01ddcaaa0edffe9
William Mak <william.mak@hotmail.ca>
2016-01-22 22:23:58 -0500

    setting an alarm, psh db? what's a db

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
-from flask import Flask
+from flask import Flask, g, request
 import json
+from time import strptime, strftime
 app = Flask(__name__)
-ignore_urls = ["/api", "/static/<path:filename>"]
+base_url = "/api/"
+ignore_urls = [base_url, "/static/<path:filename>"]
 
-@app.route("/api")
+class Alarm():
+    def __init__(self, time_format):
+        self.time = None
+        self.time_format = time_format
+
+    def set_alarm(self, time):
+        strtime = time.decode("utf-8")
+        self.time = strptime(strtime, self.time_format)
+
+    def __str__(self):
+        return strftime(self.time_format, self.time)
+
+alarm = Alarm("%H:%M")
+
+@app.route(base_url)
 def list_urls():
     urls = [rule.rule for rule in app.url_map.iter_rules() 
             if (rule.rule not in ignore_urls)]
     return json.dumps(urls)
+
+@app.route(base_url + "toggle")
+def toggle_switch():
+    return "this endpoint will eventuall toggle the light on/off"
+
+@app.route(base_url + "alarm", methods=["GET", "POST", "PUT"])
+def set_alarm():
+    if request.method == "GET":
+        return alarm.time
+    else:
+        alarm.set_alarm(request.data)
+        return "Alarm set for %s" % alarm
 
 if __name__ == "__main__":
     app.debug = True
